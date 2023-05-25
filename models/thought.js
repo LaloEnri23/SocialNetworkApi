651a93b16fa96d9612629186481e355fd107f300
LaloEnri23 <126302462+LaloEnri23@users.noreply.github.com>
2023-05-25 13:46:41 -0700

    paths

--- models/thought.js
+++ models/thought.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 // This will define Mongoose
 const { Schema, model } = require('mongoose');
 
-const reactionSchema = require('./Reaction');
+const reactionSchema = require('./reaction');
 
 //  This will define the shape of the documents within the collection.
 const thoughtSchema = new Schema(
